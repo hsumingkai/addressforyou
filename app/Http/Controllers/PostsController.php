637867835f40f4f69557c2674cd36684d6168d24
unknown <mingkai.hsu0217@gmail.com>
2019-02-26 16:39:16 +0800

    Fetching Data with Eloquent

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Post;
+use DB;
 
 class PostsController extends Controller
 {
@@ -14,9 +15,14 @@ class PostsController extends Controller
      */
     public function index()
     {
-        //
-        $posts =  Post::all();
-        
+        //take
+        // $posts =  Post::orderBy('title','desc')->take(1)->get();
+        //where
+        // $posts= Post::where('title','Post Two')->get();
+        //sql
+        // $posts = DB::select('SELECT * FROM posts');
+        $posts = Post::orderBy('title','desc')->paginate(2);
+
         return view('posts.index')->with('posts', $posts);
     }
 
@@ -27,7 +33,7 @@ class PostsController extends Controller
      */
     public function create()
     {
-        //
+        return view('posts.create');
     }
 
     /**
@@ -49,7 +55,8 @@ class PostsController extends Controller
      */
     public function show($id)
     {
-        //
+        $post = Post::find($id);
+        return view('posts.show')->with('post', $post);
     }
 
     /**
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="{{asset('css/app.css')}}">
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <title>{{config('app.name','AddressForYou')}}</title>
 
     </head>
