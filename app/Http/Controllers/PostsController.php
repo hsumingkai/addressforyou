e97788a63b80299ad620c8b5dc828435e17879f6
unknown <mingkai.hsu0217@gmail.com>
2019-02-26 17:42:43 +0800

    Forms part1

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -44,7 +44,11 @@ class PostsController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $this->validate($request, [
+            'title' => 'required',
+            'body' => 'required',
+        ]);
+        return 123;
     }
 
     /**
