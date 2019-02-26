7c617c615373657d421e8cea05c3222d0d0425bd
unknown <mingkai.hsu0217@gmail.com>
2019-02-26 22:46:23 +0800

    delete data

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -77,7 +77,9 @@ class PostsController extends Controller
      */
     public function edit($id)
     {
-        //
+        $post = Post::find($id);
+        return view('posts.edit')->with('post', $post);
+
     }
 
     /**
@@ -90,6 +92,17 @@ class PostsController extends Controller
     public function update(Request $request, $id)
     {
         //
+        $this->validate($request, [
+            'title' => 'required',
+            'body' => 'required',
+        ]);
+        $post = Post::find($id);
+        $post->title = $request->input('title');
+        $post->body = $request->input('body');
+        $post->save();
+
+        return redirect('/posts')->with('success','Post Created');
+
     }
 
     /**
@@ -100,6 +113,8 @@ class PostsController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $post = Post::find($id);
+        $post->delete();
+        return redirect('/posts')->with('success','Post Removed');
     }
 }
--- resources/views/inc/messages.blade.php
+++ resources/views/inc/messages.blade.php
@@ -7,7 +7,7 @@
     @endforeach
 @endif
 @if (session('success'))
-    <div class="alert alert-danger">
+    <div class="alert alert-success">
         {{session('success')}}
     </div>
 @endif
